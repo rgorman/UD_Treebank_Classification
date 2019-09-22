f82443972b1988a16236bc47a7127679ea7e493e
rgorman <rgorman1@unl.edu>
2019-09-22 14:09:34 -0500

    Identifies features.

--- src/preliminary_code.R
+++ src/preliminary_code.R
@@ -2,9 +2,12 @@ library(udpipe)
 vignette("udpipe-tryitout")
 library(readtext)
 library(tidyverse)
+library(magrittr)
 
 
-t <- readtext(file = "test_snippet.txt")
+files.v <- dir( pattern=".*txt")
+
+t <- readtext(file = files.v[1])
 udmodel <- udpipe_load_model(file = "german-gsd-ud-2.4-190531.udpipe" )
 
 
@@ -13,7 +16,6 @@ x <- udpipe_annotate(udmodel, x = t$text)
 x <- as.data.frame(x, detailed = TRUE)
 
 
-write.csv(x, file = "test.snipett.csv", row.names = FALSE, quote = FALSE)
 
 x$feats
 str_split(x$feats, "\\|")
@@ -23,8 +25,30 @@ str_split(x$feats, "\\|")
 max(x$sentence_id) %>%
   seq_len()
 
+output.v <- NULL
 for (i in max(x$sentence_id) %>%
      seq_len()
-     )    {
+     )    { 
+  y <- x %>%
+    filter(sentence_id == i) 
+  
+  punc_id.v <- y$token_id[which(y$upos == "PUNCT")]
+  
+ output.v <- append(output.v,  y$head_token_id %in% punc_id.v )
+  
   
 }
+
+x$head_token_id[which(x$upos == "PUNCT") ]
+
+x[which(output.v == TRUE), ] %>%
+  View()
+
+which(output.v == TRUE) %>%
+  length() %>%
+  divide_by(nrow(x))
+
+x$sentence[which(output.v == TRUE)[3]]
+
+## roughly 15 per 10,000 tokens are punctuation with dependencies.  This seems to happen in the case of a sentence
+## containing a direct quotation; the quotation marks somewhow become the head of various words in the matrix clause.
